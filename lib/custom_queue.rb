93e06d5285409da127a976971789806f88724404
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-25 17:49:52 -0400

    feat: add custom queue class

---
+++ lib/custom_queue.rb
@@ -0,0 +1,28 @@
+class CustomQueue
+  private
+
+  attr_accessor :data
+
+  def initialize
+    @data = []
+  end
+
+  public
+
+  def enqueue(object)
+    data << object
+    object
+  end
+
+  def dequeue
+    data.shift
+  end
+
+  def read
+    data[0]
+  end
+
+  def empty?
+    data.empty?
+  end
+end
