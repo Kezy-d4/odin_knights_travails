a214889d5df7c10ca16c5554b33403e555eb4560
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-26 02:05:31 -0400

    wip: knight moves

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -26,4 +26,33 @@ class KnightsTravails
     self.discovered_coordinates = CustomQueue.new
     discovered_coordinates.enqueue(start_coordinates)
   end
+
+  public
+
+  # rubocop:disable all
+  def knight_moves
+    reset
+    until discovered_coordinates.read == end_coordinates
+      current_coordinates = discovered_coordinates.read
+      adjacent_coordinates = filter_coordinates_list(possible_adjacent_coordinates(current_coordinates))
+
+      adjacent_coordinates.each do |coordinate_pair|
+        unless visited_coordinates.include?(coordinate_pair) || discovered_coordinates.include?(coordinate_pair)
+          discovered_coordinates.enqueue(coordinate_pair)
+        end
+      end
+
+      visited_coordinates << current_coordinates
+      discovered_coordinates.dequeue
+    end
+    discovered_coordinates.read
+  end
 end
+# rubocop:enable all
+
+# Driver Script
+k = KnightsTravails.new
+p k
+p k.knight_moves
+p k.knight_moves
+p k
