1b820aceab42688542cc17e2549ff905a69c4af0
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-26 01:28:06 -0400

    rework(kt): constructor

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -7,13 +7,23 @@ class KnightsTravails
   private
 
   attr_reader :start_coordinates, :end_coordinates
+  attr_accessor :visited_coordinates, :discovered_coordinates
 
   def initialize(start_coordinates = generate_coordinates, end_coordinates = generate_coordinates)
     @start_coordinates = start_coordinates
     @end_coordinates = end_coordinates
+    @visited_coordinates = []
+    @discovered_coordinates = CustomQueue.new
+    discovered_coordinates.enqueue(start_coordinates)
   end
 
   def generate_coordinates
     [rand(8), rand(8)]
+  end
+
+  def reset
+    self.visited_coordinates = []
+    self.discovered_coordinates = CustomQueue.new
+    discovered_coordinates.enqueue(start_coordinates)
   end
 end
