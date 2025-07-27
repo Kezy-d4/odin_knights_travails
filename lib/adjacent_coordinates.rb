cef0e8725f824602fdf98d008ccb9b9ce0083bac
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-27 00:08:18 -0400

    refactor: adjacent coordinates module
    Refactored for consistency in naming

--- lib/adjacent_coordinates.rb
+++ lib/adjacent_coordinates.rb
@@ -13,15 +13,15 @@ module AdjacentCoordinates
     ]
   end
 
-  def in_bounds?(coordinate_pair)
-    coordinate_pair.all? { |coordinate| coordinate.between?(0, 7) }
+  def in_bounds?(coordinates)
+    coordinates.all? { |coordinate| coordinate.between?(0, 7) }
   end
 
   def filter_coordinates_list(coordinates_list)
-    coordinates_list.select { |coordinate_pair| in_bounds?(coordinate_pair) }
+    coordinates_list.select { |coordinates| in_bounds?(coordinates) }
   end
 
-  def adjacent_coordinates(coordinate_pair)
-    filter_coordinates_list(possible_adjacent_coordinates(coordinate_pair))
+  def adjacent_coordinates(coordinates)
+    filter_coordinates_list(possible_adjacent_coordinates(coordinates))
   end
 end
