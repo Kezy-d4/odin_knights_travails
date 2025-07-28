a83a05247394b314b27dc77db2fd8ef5565dd62e
David H <151378521+Kezy-d4@users.noreply.github.com>
2025-07-27 23:29:22 -0400

    feat: add #knight_moves

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -7,23 +7,57 @@ class KnightsTravails
   private
 
   attr_reader :start_coordinates, :end_coordinates
-  attr_accessor :visited_coordinates, :discovered_coordinates
+  attr_accessor :visited, :discovered, :path
 
   def initialize(start_coordinates = generate_coordinates, end_coordinates = generate_coordinates)
     @start_coordinates = start_coordinates
     @end_coordinates = end_coordinates
-    @visited_coordinates = []
-    @discovered_coordinates = CustomQueue.new
-    discovered_coordinates.enqueue(start_coordinates)
+
+    @visited = Set.new
+    @discovered = CustomQueue.new
+    @path = { start_coordinates => nil }
+    discovered.enqueue({ coords: start_coordinates, prev: nil })
   end
 
   def generate_coordinates
     [rand(8), rand(8)]
   end
 
-  def reset
-    self.visited_coordinates = []
-    self.discovered_coordinates = CustomQueue.new
-    discovered_coordinates.enqueue(start_coordinates)
+  def bfs # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    until path.include?(end_coordinates)
+      curr = discovered.read
+      curr_coords = curr[:coords]
+      neighbors = adjacent_coordinates(curr_coords)
+
+      neighbors.each do |neighbor|
+        next if visited.include?(neighbor) || discovered.include?(neighbor)
+
+        path[neighbor] = curr_coords
+        discovered.enqueue({ coords: neighbor, prev: curr_coords })
+      end
+
+      visited << curr_coords
+      discovered.dequeue
+    end
+
+    path
+  end
+
+  def reconstruct_path(shortest_path = [end_coordinates], curr_coords = path[end_coordinates])
+    return shortest_path.reverse if curr_coords.nil?
+
+    reconstruct_path(shortest_path << curr_coords, path[curr_coords])
+  end
+
+  public
+
+  def knight_moves
+    bfs
+    shortest_path = reconstruct_path
+    puts "Your start coordinates were: #{start_coordinates}\n" \
+         "Your end coordinates were: #{end_coordinates}\n" \
+         "You reached your destination in #{shortest_path.length - 1} moves.\n" \
+         "Here was your path:\n"
+    shortest_path.each { |coords| p coords }
   end
 end
